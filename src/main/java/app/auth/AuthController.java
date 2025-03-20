00cdbcdc2b983c7d0737a787a00e50035f01b279
rorxtto <pedrohenriqueroratto@gmail.com>
2025-03-20 20:47:31 -0300

    ultima versao back

--- src/main/java/app/auth/AuthController.java
+++ src/main/java/app/auth/AuthController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/register")
+@CrossOrigin("*")
 public class AuthController {
 	
 	  private final UserService userService;
--- src/main/java/app/controller/HotmartNotificationController.java
+++ src/main/java/app/controller/HotmartNotificationController.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import org.hibernate.mapping.Map;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class HotmartNotificationController {
 		
 	 @PostMapping("/hotmart-notifications")
